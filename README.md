c712f76cec9a8da6297f56915cd47c856498b317
GutyLabs20 <gutylabs20@gmail.com>
2022-08-24 18:36:20 -0500

    execute migrate:frehs . Migration

--- README.md
+++ README.md
@@ -11,6 +11,11 @@
 
 API project for Help Desk - Support
 
+### Execute Commnads
+
+- DB migration
+    > php artisan migrate
+
 <!-- - [Simple, fast routing engine](https://laravel.com/docs/routing).
 - [Powerful dependency injection container](https://laravel.com/docs/container).
 - Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombres');
+            $table->string('apellidos');
+            $table->string('usuario');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('tipousuario_id')->nullable();
+            $table->foreign('tipousuario_id')->references('id')->on('tipousuarios')->cascadeOnDelete();
+            $table->string('activo', 1)->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
